1cf6c29fe7af21b10d0522f1c6e66ae55bd5ffb6
Gregory de la Cruz <80643448+gregorydelacruz@users.noreply.github.com>
2025-05-04 19:38:04 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import HabitDashboard from './components/HabitDashboard';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { PopupTab } from './components/PopupTab';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div className="min-h-screen bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-purple-50 via-indigo-50 to-pink-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 transition-colors duration-500">
+        <HabitDashboard />
+        <PopupTab />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
